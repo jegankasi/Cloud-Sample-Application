9caf8de6bb9cb155ae22a61be93b46ebfb20cd26
jegankasi <41259323+jegankasi@users.noreply.github.com>
2022-03-12 09:16:39 +0530

    initial commit
    spring boot sample get Service

---
+++ src/main/java/com/cloud/sample/CloudSampleApplication.java
@@ -0,0 +1,25 @@
+package com.cloud.sample;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CloudSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudSampleApplication.class, args);
+	}
+	
+	@RequestMapping("getService")
+	public List<String> getService() {
+		return Arrays.asList("Mahe", "jegan", "kumar", "hemanth");  
+	}
+
+}
---
+++ src/test/java/com/cloud/sample/CloudSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
